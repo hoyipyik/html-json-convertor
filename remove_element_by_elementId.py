18ac2d3f3ee0f0ea201f14175cc750101152bd73
hoyipyik <hoyipyik@protonmail.com>
2023-05-11 12:15:10 +0800

    remove by id

---
+++ remove_element_by_elementId.py
@@ -0,0 +1,29 @@
+import json
+
+def remove_element_by_elementId(json_obj, element_id):
+    if 'children' not in json_obj:
+        return None
+
+    for index, child in enumerate(json_obj['children']):
+        if 'elementId' in child and child['elementId'] == element_id:
+            json_obj['children'].pop(index)
+            return json_obj
+
+        updated_json = remove_element_by_elementId(child, element_id)
+        if updated_json is not None:
+            return updated_json
+
+    return {}
+
+# if run in main
+if __name__ == '__main__':
+    # read json from json.json
+    with open('json.json', 'r') as json_file:
+        json_obj = json.load(json_file)
+        new = remove_element_by_elementId(json_obj, -1236924755112662497)
+        # write to removed.json
+        with open('removed.json', 'w') as removed_file:
+            json.dump(new, removed_file, indent=4, ensure_ascii=False)
+
+
+        
